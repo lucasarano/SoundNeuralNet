c037c93e2dde0ee9a4ea021752bf93ff079eabdf
LuisL123 <luis.liuli124@gmail.com>
2024-06-13 21:44:18 -0400

    creaetd torchaudioIntro file

---
+++ torchaudioIntro.py
@@ -0,0 +1,14 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+"""
+1. Download Dataset
+2. Create data loader
+3. Build model
+4. Train
+5. Save Trained model
+"""
+
